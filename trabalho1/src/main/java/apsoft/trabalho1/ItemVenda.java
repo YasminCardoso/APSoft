2753fc50591f3ab5553ee11c2ad9abe44e53623b
YasminCardoso <yasmin.silva@eic.cefet-rj.br>
2019-04-28 11:11:15 -0300

    Questão 1 (mais ou menos)

--- trabalho1/src/main/java/apsoft/trabalho1/ItemVenda.java
+++ trabalho1/src/main/java/apsoft/trabalho1/ItemVenda.java
@@ -2,4 +2,17 @@ package apsoft.trabalho1;
 
 public class ItemVenda {
 	public Integer quantidade;
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public String toString() {
+		return quantidade.toString();
+	}
+
 }
--- trabalho1/src/main/java/apsoft/trabalho1/Produto.java
+++ trabalho1/src/main/java/apsoft/trabalho1/Produto.java
@@ -3,4 +3,24 @@ package apsoft.trabalho1;
 public class Produto {
 	public String nome;
 	public QuantiaMonetaria precoUnitario;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public QuantiaMonetaria getPrecoUnitario() {
+		return precoUnitario;
+	}
+	public void setPrecoUnitario(QuantiaMonetaria precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+	
+	
+	
 }
